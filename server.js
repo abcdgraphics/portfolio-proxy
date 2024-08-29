c5eeb451d549e5df69a1075a525761f871c8161a
VSH Kiran <saiharikiran@outlook.com>
2024-08-29 14:06:10 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import corsAnywhere from "cors-anywhere";
+const port = 8080;
+
+corsAnywhere
+  .createServer({
+    originBlacklist: [],
+    originWhitelist: [],
+    requestHeaders: ["authorization", "x-requested-with"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+  .listen(port, () => {
+    console.log(`CORS Anywhere server running on http://localhost:${port}`);
+  });
